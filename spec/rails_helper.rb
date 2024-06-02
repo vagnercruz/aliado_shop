bfb0cc7f95384e907d8b03d6886e38a0c743206f
vagnercruz <vagnerdacruz48@gmail.com>
2024-06-02 13:53:23 -0300

    Configurando o Faker

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 require 'vcr'
 require 'simplecov'
+require 'faker'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 abort("The Rails environment is running in production mode!") if Rails.env.production?

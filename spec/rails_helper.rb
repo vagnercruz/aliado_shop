a538de410a9d45bebcebd181b639394cb5f241df
vagnercruz <vagnerdacruz48@gmail.com>
2024-06-02 05:27:47 -0300

    Configuração vcr

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'vcr'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 abort("The Rails environment is running in production mode!") if Rails.env.production?
@@ -29,4 +30,9 @@ Shoulda::Matchers.configure do |config|
     with.test_framework :rspec
     with.library :rails
   end
+end
+
+VCR.configure do |config|
+  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.hook_into :webmock
 end
